c2dc235e68107b3413ff69620e2f36945e870001
友人a丶 <1747111677@qq.com>
2022-07-24 23:28:36 +0800

    2022-07-24

--- template/widget/info.php
+++ template/widget/info.php
@@ -5,19 +5,23 @@
  * @date 2022-07-08
  * */
 global $post;
-$author   = get_the_author_meta( 'display_name', $post->post_author );
+$author = get_the_author_meta( 'display_name', $post->post_author );
+if ( is_single() ) {
+	?>
+    <!--文章信息-->
+    <div class="div-info">
+        <div class="header">
+            <ul>
+                <li class="active">文章信息</li>
+                <!-- <li>修改记录</li>-->
+            </ul>
+        </div>
+        <ul class="ul">
+            <li><?= $author; ?>创建于：<span><?= the_time( "Y-m-d H:i" ); ?></span></li>
+            <li>最后编辑于：<span><?= get_post_modified_time( "Y-m-d H:i" ); ?></span></li>
+            <li>字数统计：<span><?= mb_strlen( get_the_content() ); ?></span></li>
+        </ul>
+    </div>
+	<?php
+}
 ?>
-<!--文章信息-->
-<div class="div-info">
-	<div class="header">
-		<ul>
-			<li class="active">文章信息</li>
-			<!-- <li>修改记录</li>-->
-		</ul>
-	</div>
-	<ul class="ul">
-		<li><?=$author;?>创建于：<span><?=the_time("Y-m-d H:i");?></span></li>
-		<li>最后编辑于：<span><?=get_post_modified_time("Y-m-d H:i");?></span></li>
-		<li>字数统计：<span><?=mb_strlen(get_the_content()); ?></span></li>
-	</ul>
-</div>
