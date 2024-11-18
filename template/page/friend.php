514de486ee946fd8a585bde568709baa7dc9cb7c
友人a丶 <1747111677@qq.com>
2024-11-18 17:58:19 +0800

    优化图片加载

--- template/page/friend.php
+++ template/page/friend.php
@@ -1,29 +1,7 @@
 <?php
 
 /*
- * 留言的模板页面
+ * 友情链接页面模板
  * @author 友人a丶
- * @date 2022-07-08
+ * @date 2024-11-18
  * */
-nicen_theme_setPostViews( get_the_ID() );//阅读次数加1
-get_header();//加载顶部内容
-?>
-<main class="main-container no-sidebar">
-    <!--  文章  -->
-    <div class="main-main">
-        <article class="main-content">
-			<?php get_template_part( './template/index/article-header' ); ?>
-            <!--  文章内容  -->
-            <div class="main-article">
-				<?php echo get_the_content(); ?>
-            </div>
-			<?php get_template_part( './template/index/fixed' ); ?>
-        </article>
-		<?php comments_template(); ?>
-    </div>
-</main>
-<?php
-get_footer();
-?>
-
-
